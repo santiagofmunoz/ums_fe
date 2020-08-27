e46268b779c574027bf5d58d31a921daf3194f0f
Santiago Muñoz <santiagofmunoz@gmail.com>
2020-08-27 19:10:35 -0300

    NEW: Created left menu associated to header (Responsive). NEW: Added React Router to manage navigation. UPD: Updated dependencies.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,132 @@
 import React from 'react'
-import AppBar from "@material-ui/core/AppBar";
-import IconButton from "@material-ui/core/IconButton";
+import { AppBar,
+    CssBaseline,
+    Drawer,
+    Divider,
+    Hidden,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    makeStyles,
+    Toolbar,
+    Typography,
+    useMediaQuery,
+    useTheme } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu"
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import {makeStyles} from "@material-ui/core/styles";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        display: 'flex',
         flexGrow: 1,
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        }
     },
     menuButton: {
         marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
     },
     title: {
         flexGrow: 1,
     },
     appBar: {
+        zIndex: theme.zIndex.drawer + 1,
         background: "#245b80"
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
     }
 }));
 
 function Header(props) {
+    const { window } = props;
     const classes = useStyles();
+    const theme = useTheme();
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const headerText = useMediaQuery(theme.breakpoints.up('sm')) ? "University Management System" : "UMS";
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div>
+            <div className={classes.toolbar} />
+            <Divider />
+            <List>
+                {['Item 1', 'Item 2', 'Item 3', 'Item 4'].map((text) => (
+                    <ListItem button key = {text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['Cerrar Sesión'].map((text) => (
+                    <ListItem button key = {text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
         <div className={classes.root}>
-            <AppBar position="static" className={classes.appBar}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon open />
+                    <IconButton edge="start" onClick={handleDrawerToggle} className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        UMS
+                        {headerText}
                     </Typography>
                 </Toolbar>
             </AppBar>
+            <nav className={classes.drawer} aria-label="actions-menu">
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        container={container}
+                        variant="temporary"
+                        anchor={theme.direction === "rtl" ? "right" : "left"}
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                        ModalProps={{
+                            keepMounted: true,
+                        }}
+                    >
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer
+                        classes={{paper: classes.drawerPaper}}
+                        variant="permanent"
+                        open
+                    >
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+            </nav>
         </div>
     )
 }
